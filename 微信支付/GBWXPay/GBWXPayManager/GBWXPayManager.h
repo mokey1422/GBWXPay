c44673a032e7e3c1d787ed40c29cc4bd54db2e1b
mokey1422 <15253503932@163.com>
2015-10-16 16:00:36 +0800

    zhang

--- 微信支付/GBWXPay/GBWXPayManager/GBWXPayManager.h
+++ 微信支付/GBWXPay/GBWXPayManager/GBWXPayManager.h
@@ -12,9 +12,9 @@
 /**
  *  使用说明
  *1、直接把GBWXPay文件拖到你的工程里面
- *2、配置一下系统库libsqlite3.0.dylib、libz.dzlib、SystemConfiguration.frame添加到自己的项目中去，编译一下如果没有问题，就进行下一步，如果有问题可能提示查无此类，不要紧在相应的类里面包含一下#import <UIKit/UIKit.h>就ok了，如果你的pch里面包含了那就更好了这个地方就不会报错
+ *2、配置一下系统库libsqlite3.0.dylib、libz.dzlib、CoreTelephony.framework、SystemConfiguration.frame添加到自己的项目中去，编译一下如果没有问题，就进行下一步，如果有问题可能提示查无此类，不要紧在相应的类里面包含一下#import <UIKit/UIKit.h>就ok了，如果你的pch里面包含了那就更好了这个地方就不会报错
  *3、编译无错的话那就在应用的地方包含一下我的管理文件的头文件#import "GBWXPayManager.h"，直接调用里面的方法实现支付功能
- *4、配置appdelegate,先包含#import "GBWXPayManager.h"头文件，此外注意微信支付调用回调的时候用的时协议代理，这里要在appdelegate里面包含一下代理协议，复制以下代码：
+ *4、配置appdelegate,先包含#import "GBWXPayManager.h"头文件，此外注意微信支付调用回调的时候用的时*协议代理，这里要在appdelegate里面包含一下代理协议，复制以下代码：
  
  - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
  
@@ -61,6 +61,21 @@
  
  *6、接下来就要设置回调参数，实现app支付完成的回调了
   点击项目名称,点击“Info”选项卡，在URL types里面添加一项，Identifier可以不填，URL schemes就是appid直接复制进去就ok了；
+ *7、iOS 9系统策略更新，限制了http协议的访问，此外应用需要在“Info.plist”中将要使用的URL Schemes列为白名单，才可正常检查其他应用是否安装。
+ *   在“Info.plist”里增加如下代码：
+ 
+ <key>LSApplicationQueriesSchemes</key>
+ <array>
+ <string>weixin</string>
+ </array>
+ 
+ <key>NSAppTransportSecurity</key>
+ <dict>
+ <key>NSAllowsArbitraryLoads</key>
+ <true/>
+ </dict>
+ *8、解决bitcode编译不过问题
+ *   在Build Setting将Enable Bitcode 设置为No
  */
 
 @interface GBWXPayManager : NSObject
@@ -75,7 +90,12 @@
 +(void)wxpayWithOrderID:(NSString*)orderID
              orderTitle:(NSString*)orderTitle
                  amount:(NSString*)amount
-                seller :(NSString *)seller;
+               sellerID:(NSString *)sellerID
+                  appID:(NSString*)appID
+              partnerID:(NSString*)partnerID
+
+;
+
 /**
  *  单一用户
  *
--- 微信支付/GBWXPay/GBWXPayManager/GBWXPayManager.m
+++ 微信支付/GBWXPay/GBWXPayManager/GBWXPayManager.m
@@ -21,10 +21,52 @@
 +(void)wxpayWithOrderID:(NSString*)orderID
              orderTitle:(NSString*)orderTitle
                  amount:(NSString*)amount
-                seller :(NSString *)seller{
+               sellerID:(NSString *)sellerID
+                  appID:(NSString*)appID
+              partnerID:(NSString*)partnerID{
     
+    //微信支付的金额单位是分转化成我们比较常用的'元'
+    NSString*realPrice=[NSString stringWithFormat:@"%.f",amount.floatValue*100];
     
+    if(realPrice.floatValue<=0){
+        return;
+    }
+    //创建支付签名对象
+    payRequsestHandler *req = [payRequsestHandler alloc];
+    //初始化支付签名对象
+    [req init:appID mch_id:sellerID];
+    //设置密钥
+    [req setKey:partnerID];
     
+    //获取到实际调起微信支付的参数后，在app端调起支付
+    NSMutableDictionary *dict = [req sendPay_demo:orderID title:orderTitle price: realPrice];
+    
+    if(dict == nil){
+        //错误提示
+        NSString *debug = [req getDebugifo];
+        
+        [self alert:@"提示信息" msg:debug];
+        
+        NSLog(@"%@\n\n",debug);
+    }else{
+        NSLog(@"%@\n\n",[req getDebugifo]);
+        
+        NSMutableString *stamp  = [dict objectForKey:@"timestamp"];
+        
+        //调起微信支付
+        PayReq* req             = [[PayReq alloc] init];
+        req.openID              = [dict objectForKey:@"appid"];
+        req.partnerId           = [dict objectForKey:@"partnerid"];
+        req.prepayId            = [dict objectForKey:@"prepayid"];
+        req.nonceStr            = [dict objectForKey:@"noncestr"];
+        req.timeStamp           = stamp.intValue;
+        req.package             = [dict objectForKey:@"package"];
+        req.sign                = [dict objectForKey:@"sign"];
+        
+        BOOL status = [WXApi sendReq:req];
+        NSLog(@"%d",status);
+        
+    }
     
 }
 /**
